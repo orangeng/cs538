8b89a6f65fe98b79e6f1a8f4a8d186c0c4973385
orangeng <ngquanhao@outlook.com>
2025-03-10 21:38:18 -0500

    Mininet setup

---
+++ mininet.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Pull in mininet
+git clone https://github.com/mininet/mininet
+cd mininet
+git checkout -b mininet-2.3.0 2.3.0
+sed -i 's|git://|https://|g' ./util/install.sh # Because sometimes git clone git:// fails
+
+# Install
+cd ~
+./mininet/util/install.sh -a
+
+# Run test
+sudo mn --switch ovsbr --test pingall
